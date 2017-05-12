a82943c0bd7714b216edf72dcfdfc0504bab909c
Taloth Saldono <Taloth@users.noreply.github.com>
2017-05-12 21:41:05 +0200

    Refactored IndexerStatusService into Thingy Provider architecture.

--- src/NzbDrone.Core.Test/IndexerTests/IndexerStatusServiceFixture.cs
+++ src/NzbDrone.Core.Test/IndexerTests/IndexerStatusServiceFixture.cs
@@ -5,13 +5,14 @@ using Moq;
 using NUnit.Framework;
 using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.ThingiProvider.Status;
 
 namespace NzbDrone.Core.Test.IndexerTests
 {
     public class IndexerStatusServiceFixture : CoreTest<IndexerStatusService>
     {
         private DateTime _epoch;
-        
+
         [SetUp]
         public void SetUp()
         {
@@ -21,7 +22,7 @@ namespace NzbDrone.Core.Test.IndexerTests
         private void WithStatus(IndexerStatus status)
         {
             Mocker.GetMock<IIndexerStatusRepository>()
-                .Setup(v => v.FindByIndexerId(1))
+                .Setup(v => v.FindByProviderId(1))
                 .Returns(status);
 
             Mocker.GetMock<IIndexerStatusRepository>()
@@ -44,7 +45,7 @@ namespace NzbDrone.Core.Test.IndexerTests
 
             VerifyUpdate();
 
-            var status = Subject.GetBlockedIndexers().FirstOrDefault();
+            var status = Subject.GetBlockedProviders().FirstOrDefault();
             status.Should().NotBeNull();
             status.DisabledTill.Should().HaveValue();
             status.DisabledTill.Value.Should().BeCloseTo(_epoch + TimeSpan.FromMinutes(5), 500);
@@ -59,7 +60,7 @@ namespace NzbDrone.Core.Test.IndexerTests
 
             VerifyUpdate();
 
-            var status = Subject.GetBlockedIndexers().FirstOrDefault();
+            var status = Subject.GetBlockedProviders().FirstOrDefault();
             status.Should().BeNull();
         }
 
@@ -82,7 +83,7 @@ namespace NzbDrone.Core.Test.IndexerTests
             Subject.RecordSuccess(1);
             Subject.RecordFailure(1);
 
-            var status = Subject.GetBlockedIndexers().FirstOrDefault();
+            var status = Subject.GetBlockedProviders().FirstOrDefault();
             status.Should().NotBeNull();
             status.DisabledTill.Should().HaveValue();
             status.DisabledTill.Value.Should().BeCloseTo(_epoch + TimeSpan.FromMinutes(15), 500);
